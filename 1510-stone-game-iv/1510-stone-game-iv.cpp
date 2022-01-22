fff926132893c23babe9f96b4587e1b2e096237e
khalid farooq <47777912+khalidfarooq@users.noreply.github.com>
2022-01-23 00:05:27 +0530

    Time: 43 ms (67.79%), Space: 6.2 MB (89.60%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    bool dfs(unordered_map<int,bool>&m, int left){
-        if(m.find(left) != m.end()){
-            return m[left];
-        }
+//     bool dfs(unordered_map<int,bool>&m, int left){
+//         if(m.find(left) != m.end()){
+//             return m[left];
+//         }
         
-        int sqt = (int)sqrt(left);
-        for(int i=1; i<=sqt; i++){
-            if(!dfs(m, left-i*i)){
-                m[left] = true;
-                return true;
+//         int sqt = (int)sqrt(left);
+//         for(int i=1; i<=sqt; i++){
+//             if(!dfs(m, left-i*i)){
+//                 m[left] = true;
+//                 return true;
+//             }
+//         }
+//         m[left] = false;
+//         return false;
+//     }
+    bool winnerSquareGame(int n) {
+        vector<bool>dp(n+1);
+        for(int i=0; i< n+1; i++){
+            for(int k = 1; k*k<=i; k++){
+                if(dp[i-k*k] == false){
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        m[left] = false;
-        return false;
-    }
-    bool winnerSquareGame(int n) {
-        unordered_map<int,bool>m;
-        m[0] = false;
-        return dfs(m,n);
+        return dp[n];
+        // unordered_map<int,bool>m;
+        // m[0] = false;
+        // return dfs(m,n);
     }
 };
 
