0387d8c72559a9c6bb6e13d7433454d6b63a797d
khalid farooq <47777912+khalidfarooq@users.noreply.github.com>
2022-01-26 12:41:55 +0530

    Time: 307 ms (7.46%), Space: 85.3 MB (58.22%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -12,27 +12,47 @@
 class Solution {
 public:
     vector<int>res;
-    void h2(TreeNode *r){
-        if(r == NULL) return;
-        h1(r->left);
-        res.push_back(r->val);
-        h1(r->right);
+    
+//     void dfs(TreeNode *r){
+//         if(r == NULL) return;
+//         dfs(r->left);
+//         res.push_back(r->val);
+//         dfs(r->right);
         
-        return;
-    }
-    void h1(TreeNode *r){
-        if(r == NULL) return;
-        h1(r->left);
-        res.push_back(r->val);
-        h1(r->right);
-        
-        return;
-    }
+//         return;
+//     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        stack<TreeNode *>s1,s2;
+        if(root1 == NULL && root2 == NULL) return res;
         
-        h1(root1);
-        h2(root2);
-        sort(begin(res), end(res));
+        TreeNode * r1 = root1;
+        TreeNode * r2 = root2;
+        while(!s1.empty() || !s2.empty() || r1 != NULL || r2 != NULL){
+            while(r1 != NULL){
+                s1.push(r1);
+                r1 = r1->left;
+            }
+            while(r2 != NULL){
+                s2.push(r2);
+                r2 = r2->left;
+            }
+            int x = (!s1.empty()) ? s1.top()->val : INT_MAX;
+            int y = (!s2.empty()) ? s2.top()->val : INT_MAX;
+            
+            if(x < y){
+                res.push_back(x);
+                r1 = s1.top()->right;
+                s1.pop();
+            }else{
+                res.push_back(y);
+                r2 = s2.top()->right;
+                s2.pop();
+            }
+        }
+        
+        // dfs(root1);
+        // dfs(root2);
+        // sort(begin(res), end(res));
         return res;
     }
 };
