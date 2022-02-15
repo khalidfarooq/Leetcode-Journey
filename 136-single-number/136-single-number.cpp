6de82ca0c45c8efcc731b18779d7d4171bd25852
khalid farooq <47777912+khalidfarooq@users.noreply.github.com>
2022-02-15 21:16:56 +0530

    Time: 47 ms (14.15%), Space: 20.8 MB (6.42%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int>m;
+        for(int x : nums) m[x]++;
+        
+        for(auto x : m){
+            if(x.second == 1) return x.first;
+        }
+        return -1;
+    }
+};
